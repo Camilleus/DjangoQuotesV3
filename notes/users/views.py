a3ecc27c96b8fe761546e04c2d11ffab38212c36
CamilleusRex <camilleusrex@gmail.com>
2024-01-19 21:31:35 +0100

    added login

--- notes/users/views.py
+++ notes/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import RegisterForm
+
 
 def signupuser(request):
     if request.user.is_authenticated:
@@ -15,3 +17,19 @@ def signupuser(request):
             return render(request, 'users/signup.html', context={"form": form})
 
     return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='noteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='noteapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
