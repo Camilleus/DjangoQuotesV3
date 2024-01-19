5828d83d278c46608abfd537f7797d8b4c8d21c3
CamilleusRex <camilleusrex@gmail.com>
2024-01-19 21:40:43 +0100

    added logout

--- notes/users/views.py
+++ notes/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 def signupuser(request):
@@ -33,3 +35,9 @@ def loginuser(request):
         return redirect(to='noteapp:main')
 
     return render(request, 'users/login.html', context={"form": LoginForm()})
+
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='noteapp:main')
