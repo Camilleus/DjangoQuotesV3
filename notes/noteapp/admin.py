21b948c84bc105bdb49dc61aea1fbd442490b86a
CamilleusRex <camilleusrex@gmail.com>
2024-01-19 23:47:15 +0100

    fixed everything, now it works properly

--- notes/noteapp/admin.py
+++ notes/noteapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Tag, Note, Profile
+from .models import Tag, Note
 
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Note)
-admin.site.register(Profile)
--- notes/users/views.py
+++ notes/users/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, ProfileForm
 
 
 def signupuser(request):
