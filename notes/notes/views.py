64b81023a1da1bfb486abc3696bb63d2ea32b3fb
CamilleusRex <camilleusrex@gmail.com>
2024-01-19 15:39:28 +0100

    Create views.py

---
+++ notes/notes/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Author, Quote
+from .forms import AuthorForm, QuoteForm
+
+@login_required
+def add_author(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('authors_list')
+    else:
+        form = AuthorForm()
+    return render(request, 'add_author.html', {'form': form})
+
+@login_required
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            quote = form.save(commit=False)
+            quote.user = request.user
+            quote.save()
+            return redirect('quotes_list')
+    else:
+        form = QuoteForm()
+    return render(request, 'add_quote.html', {'form': form})
