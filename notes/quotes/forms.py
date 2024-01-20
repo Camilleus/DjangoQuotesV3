0028ce211952b161ca7378cc8f117571e6e5d77b
CamilleusRex <camilleusrex@gmail.com>
2024-01-20 18:23:04 +0100

    added profile of author

--- notes/quotes/forms.py
+++ notes/quotes/forms.py
@@ -6,7 +6,20 @@ class QuoteForm(forms.ModelForm):
         model = Quote
         fields = ['text', 'author']
 
+
 class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
-        fields = ['name']
+        fields = ['name', 'description', 'image']
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if not description:
+            raise forms.ValidationError('This field is required.')
+        return description
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        if not image:
+            raise forms.ValidationError('This field is required.')
+        return image
--- notes/quotes/models.py
+++ notes/quotes/models.py
@@ -3,6 +3,8 @@ from users.models import Profile
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='author_images/')
 
     def __str__(self):
         return self.name
