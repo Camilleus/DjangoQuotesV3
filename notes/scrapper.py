36beb87331c255ddfad0630d679eaac764ba059c
CamilleusRex <camilleusrex@gmail.com>
2024-01-21 12:36:06 +0100

    added saving data to DB

--- notes/scrapper.py
+++ notes/scrapper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import sqlite3
 
 def scrape_quotes(url):
     response = requests.get(url)
@@ -22,9 +23,26 @@ def scrape_quotes(url):
 
         return data
 
+def save_to_database(quotes_data, db_name='sqlite3.db'):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute('CREATE TABLE IF NOT EXISTS quotes (quote TEXT, author TEXT)')
+
+    for quote_info in quotes_data:
+        quote_text = quote_info['quote']
+        author_name = quote_info['author']
+
+        c.execute('INSERT INTO quotes (quote, author) VALUES (?, ?)', (quote_text, author_name))
+
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
     url = "https://quotes.toscrape.com"
     quotes_data = scrape_quotes(url)
 
     with open('quotes.json', 'w', encoding='utf-8') as json_file:
         json.dump(quotes_data, json_file, ensure_ascii=False, indent=2)
+
+    save_to_database(quotes_data, 'sqlite3.db')
